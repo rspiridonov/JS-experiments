dcfc8fc0bf1ea4335befb9b068908f22fc627931
evilexp <evilexp@gmail.com>
2012-02-13 02:21:15 +0400

    add testing of mouse click coordinates

--- game_loop/script.js
+++ game_loop/script.js
@@ -24,3 +24,25 @@ Game.update = function() {
 Game.addRect = function() {
   Game.entities.push(new Rect());
 };
+
+function getCursorPosition(e, canvas_el) {
+  var x;
+  var y;
+  if (e.pageX || e.pageY) {
+    x = e.pageX;
+    y = e.pageY;
+  }
+  else {
+    x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+    y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+  }
+  x -= canvas_el.offsetLeft;
+  y -= canvas_el.offsetTop;
+
+  return [x, y];
+}
+
+function testClick(e) {
+  var canvas_el = document.getElementById("viewport");
+  var cell = getCursorPosition(e, canvas_el);
+}
